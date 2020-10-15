151232ac9b248cd8d8af15b2b0287399975480e1
cybertekschool <github@cybertekschool.com>
2020-10-14 20:39:27 -0400

    Java Review

--- src/main/java/com/cybertek/streams/IntermediateOperations.java
+++ src/main/java/com/cybertek/streams/IntermediateOperations.java
@@ -1,6 +1,7 @@
 package com.cybertek.streams;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class IntermediateOperations {
@@ -12,7 +13,7 @@ public class IntermediateOperations {
 
         System.out.println("***********FILTER***********");
 
-        //fILTER
+        //FILTER
         System.out.println(numbersStream.filter(number -> number%2 !=0).count());
 
         Stream<String> strStream = Arrays.asList("Apple","Orange","Kiwi","Banana", "Apple","Cheery","Mango").stream();
@@ -21,6 +22,23 @@ public class IntermediateOperations {
         //System.out.println(strStream.filter(str -> str.startsWith("A")).count()); //ERROR
 
         Arrays.asList("Apple","Orange","Kiwi","Banana", "Apple","Cheery","Mango").stream().filter(str -> str.equals("Apple")).forEach(str -> System.out.println(str));
+
+        System.out.println("***********MAP***********");
+        Arrays.asList("Apple","Orange","Kiwi","Banana", "Apple","Cheery","Mango").stream().map(str -> str.toUpperCase()).forEach(str -> System.out.println(str));
+
+        System.out.println("***********DISTINCT***********");
+        Arrays.asList("Apple","Orange","Kiwi","Banana", "Apple","Cheery","Mango").stream().distinct().forEach(str -> System.out.println(str));
+
+        System.out.println("***********SORTED ORDER***********");
+        Arrays.asList(1,2,3,4,6,5,7,8,9).stream().sorted().forEach(number -> System.out.println(number));
+
+        System.out.println("***********SORTED REVERSED ORDER***********");
+        Arrays.asList(1,2,3,4,6,5,7,8,9).stream().sorted(Comparator.reverseOrder()).forEach(number -> System.out.println(number));
+
+
+
+
+
 
 
 
